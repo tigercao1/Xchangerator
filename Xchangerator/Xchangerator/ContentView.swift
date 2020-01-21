1e5210f3fd72f1906ed97bed4bb9543e90c4e446
zywkloo <zyw870315@163.com>
2020-01-21 01:15:09 -0500

    add 4 tabs

---
+++ Xchangerator/Xchangerator/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  Xchangerator
+//
+//  Created by 张一唯 on 2020-01-21.
+//  Copyright © 2020 YYES. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("Home View")
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .lineLimit(nil)
+                .padding()
+                .tabItem {
+                    VStack {
+                        Image("icon")
+                        .resizable()
+                        .frame(width: 100,
+                               height: 100,
+                               alignment: .center)
+                        Text("Home")
+                            .font(.title)
+                    }
+                }
+                .tag(0)
+            Text("Favorite View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("Favorite")
+                        Text("Favorite")
+                        .font(.title)
+                    }
+                }
+                .tag(1)
+            Text("Notification View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("Notification")
+                        Text("Notification")
+                    }
+                }
+                .tag(2)
+            Text("Settings View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("Settings")
+                        Text("Settings")
+                    }
+                }
+                .tag(3)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
