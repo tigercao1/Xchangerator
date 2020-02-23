c9e0547a04eb15944f33215ca1b6c9dda01e89a9
zywkloo <zyw870315@163.com>
2020-02-23 17:52:36 -0500

    build works after pull

--- Xchangerator/Xchangerator/AppDelegate.swift
+++ Xchangerator/Xchangerator/AppDelegate.swift
@@ -35,16 +35,16 @@ class AppDelegate: UIResponder,UIApplicationDelegate,FUIAuthDelegate,MessagingDe
         // [START register_for_notifications]
         if #available(iOS 10.0, *) {
           // For iOS 10 display notification (sent via APNS)
-            UNUserNotificationCenter.current().delegate = self
+            UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
 
-          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-          UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: {_, _ in })
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
         } else {
-          let settings: UIUserNotificationSettings =
-          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-          application.registerUserNotificationSettings(settings)
+            let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
         }
 
         application.registerForRemoteNotifications()
